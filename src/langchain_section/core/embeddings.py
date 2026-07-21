b77f923d1c37b3879673f0612af195c74273c227
Tomás R. Pita <tomasrpita@gmail.com>
2026-07-21 05:51:53 +0200

    feat(embeddings): add get_or_create_vectorstore function for managing vector stores

---
+++ src/langchain_section/core/embeddings.py
@@ -0,0 +1,18 @@
+import os
+from langchain_chroma import Chroma
+from src.langchain_section.core.llm import get_embeddings
+from src.langchain_section.config.settings import settings
+
+
+def get_or_create_vectorstore(
+        collection_name: str,
+        persist_path: str | None = None
+) -> Chroma
+    """Obtener un vector store existente"""
+    path = persist_path or settings.CHROMA_PATH
+    os.makedirs(path, exist_ok=True)
+
+    return Chroma(
+        collection_name=collection_name,
+        embedding_function=get_embeddings()
+    )
