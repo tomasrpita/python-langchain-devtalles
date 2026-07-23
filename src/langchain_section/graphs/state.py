f675665a0327328e7bd5a64655671279a769dd2f
Tomás R. Pita <tomasrpita@gmail.com>
2026-07-23 06:53:24 +0200

    feat(dependencies): add langgraph  dependencie; u feat(state): create RAGAgenticState TypedDict for structured state management

---
+++ src/langchain_section/graphs/state.py
@@ -0,0 +1,14 @@
+from typing import Annotated, TypedDict
+
+from langgraph.graph import add_messages
+
+
+class RAGAgenticState(TypedDict):
+    "Estados"
+
+    messages: Annotated[list, add_messages]
+    question: str
+    retrived_docs: list[str]
+    response: str
+    needs_retrieval: bool
+    sources: list[dict]
